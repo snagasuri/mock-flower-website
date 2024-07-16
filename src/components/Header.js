44db3830276fab2cc711a95c5988fda343ddbb4d
snagasuri <ramnag2003@gmail.com>
2024-07-16 11:28:42 -0700

    Created a new React component for the application header.

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Search, ShoppingCart, Menu } from 'lucide-react';
+
+const Header = () => (
+  <header className="flex justify-between items-center p-4 bg-pink-300 text-white">
+    <nav className="flex space-x-4">
+      <Link to="/" className="hover:underline">home</Link>
+      <Link to="/shop" className="hover:underline">shop</Link>
+      <Link to="/about" className="hover:underline">about</Link>
+    </nav>
+    <div className="text-xl font-bold lowercase absolute left-1/2 transform -translate-x-1/2">
+      margaret's
+    </div>
+    <div className="flex items-center space-x-4">
+      <Search className="h-6 w-6 cursor-pointer" />
+      <ShoppingCart className="h-6 w-6 cursor-pointer" />
+      <Menu className="h-6 w-6 cursor-pointer" />
+    </div>
+  </header>
+);
+
+export default Header;
